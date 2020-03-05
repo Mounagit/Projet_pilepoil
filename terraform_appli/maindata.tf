68c086b821008563fc7e423101dd9776ab68f853
vagrant <vagrant@localhost.localdomain>
2020-03-05 13:39:31 +0000

    ajout fichier maindata.tf

---
+++ terraform_appli/maindata.tf
@@ -0,0 +1,34 @@
+
+# création du ressource group
+data "azurerm_resource_group" "RGapp" {
+    name = "${var.nameRG}"
+    location = "${var.location}" 
+}
+
+# création du Azure Vnet 
+
+data "azurerm_virtual_network" "my_AzureVnet" {
+    name = "${var.nameVnet}" 
+    address_space = [ "10.0.0.0/16" ]
+    location = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.RGapp.name}"
+    
+}
+
+# création du subnet 
+data "azurerm_subnet" "subnet_test" {
+    name = "subnet_test"
+    resource_group_name = "${azurerm_resource_group.RGapp.name}"
+    virtual_network_name = "${azurerm_virtual_network.my_AzureVnet.name}"
+    address_prefix = "10.0.3.0/24"
+}
+
+
+# création du subnet 
+data "azurerm_subnet" "subnet_prod" {
+    name = "subnet_prod"
+    resource_group_name = "${azurerm_resource_group.RGapp.name}"
+    virtual_network_name = "${azurerm_virtual_network.my_AzureVnet.name}"
+    address_prefix = "10.0.2.0/24"
+
+}
