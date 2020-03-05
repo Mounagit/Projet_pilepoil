64d6ac6ef1ed44a74fec2965f3dda8bd3ffac1ae
Mounabal <60136215+Mounagit@users.noreply.github.com>
2020-03-05 15:11:15 +0100

    Update maindata.tf

--- terraform_appli/maindata.tf
+++ terraform_appli/maindata.tf
@@ -11,15 +11,15 @@ data "azurerm_virtual_network" "my_AzureVnet" {
     name = "${var.nameVnet}" 
     address_space = [ "10.0.0.0/16" ]
     location = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.RGapp.name}"
+    resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
     
 }
 
 # création du subnet 
 data "azurerm_subnet" "subnet_test" {
     name = "subnet_test"
-    resource_group_name = "${azurerm_resource_group.RGapp.name}"
-    virtual_network_name = "${azurerm_virtual_network.my_AzureVnet.name}"
+    resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
+    virtual_network_name = "${data.azurerm_virtual_network.my_AzureVnet.name}"
     address_prefix = "10.0.3.0/24"
 }
 
@@ -27,8 +27,8 @@ data "azurerm_subnet" "subnet_test" {
 # création du subnet 
 data "azurerm_subnet" "subnet_prod" {
     name = "subnet_prod"
-    resource_group_name = "${azurerm_resource_group.RGapp.name}"
-    virtual_network_name = "${azurerm_virtual_network.my_AzureVnet.name}"
+    resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
+    virtual_network_name = "${data.azurerm_virtual_network.my_AzureVnet.name}"
     address_prefix = "10.0.2.0/24"
 
 }
