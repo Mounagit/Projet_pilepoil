e100f9b98ba195a0e87951d059f1349e64040f63
Mounabal <60136215+Mounagit@users.noreply.github.com>
2020-03-05 18:56:48 +0100

    Update main.tf

--- terraform_appli/main.tf
+++ terraform_appli/main.tf
@@ -13,20 +13,20 @@ data "azurerm_subnet" "subnet_test" {
     #address_prefix = "10.0.3.0/24"
 }
 
-# création du subnet 
-data "azurerm_subnet" "subnet_prod" {
-    name = "subnet_prod"
-    resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
-    virtual_network_name = "${data.azurerm_virtual_network.my_AzureVnet.name}"
+  # création du subnet 
+    #data "azurerm_subnet" "subnet_prod" {
+    #name = "subnet_prod"
+    #resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
+    #virtual_network_name = "${data.azurerm_virtual_network.my_AzureVnet.name}"
     #address_prefix = "10.0.2.0/24"
 
-}
+#}
 
 
 
 
 #############################
-####Créatino de la VM test
+####Création de la VM test
 
 
 
