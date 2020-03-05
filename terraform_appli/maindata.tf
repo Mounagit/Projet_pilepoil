b8c638a1f29f2fc9f086380e5549ea3d2cfd2348
Mounabal <60136215+Mounagit@users.noreply.github.com>
2020-03-05 15:24:37 +0100

    Update maindata.tf

--- terraform_appli/maindata.tf
+++ terraform_appli/maindata.tf
@@ -1,16 +1,16 @@
 
 # création du ressource group
 data "azurerm_resource_group" "RGapp" {
-    name = "${var.nameRG}"
-    location = "${var.location}" 
+    name = "${var.nameRG}" 
+    #location = "${var.location}"
 }
 
 # création du Azure Vnet 
 
 data "azurerm_virtual_network" "my_AzureVnet" {
     name = "${var.nameVnet}" 
-    address_space = [ "10.0.0.0/16" ]
-    location = "${var.location}"
+    #address_space = [ "10.0.0.0/16" ]
+    #location = "${var.location}"
     resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
     
 }
@@ -20,7 +20,7 @@ data "azurerm_subnet" "subnet_test" {
     name = "subnet_test"
     resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
     virtual_network_name = "${data.azurerm_virtual_network.my_AzureVnet.name}"
-    address_prefix = "10.0.3.0/24"
+    #address_prefix = "10.0.3.0/24"
 }
 
 
@@ -29,6 +29,6 @@ data "azurerm_subnet" "subnet_prod" {
     name = "subnet_prod"
     resource_group_name = "${data.azurerm_resource_group.RGapp.name}"
     virtual_network_name = "${data.azurerm_virtual_network.my_AzureVnet.name}"
-    address_prefix = "10.0.2.0/24"
+    #address_prefix = "10.0.2.0/24"
 
 }
